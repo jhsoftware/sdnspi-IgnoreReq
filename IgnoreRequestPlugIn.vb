443e3f409e8af035bda524ff1d3ff744a5b3a310
Jesper Høy <support@jhsoftware.dk>
2015-10-14 11:33:00 +0200

    First commit

---
+++ IgnoreRequestPlugIn.vb
@@ -0,0 +1,54 @@
+﻿Public Class IgnoreRequestPlugIn
+  Implements JHSoftware.SimpleDNS.Plugin.IIgnoreRequestPlugIn
+
+#Region "events"
+  Public Event AsyncError(ByVal ex As System.Exception) Implements JHSoftware.SimpleDNS.Plugin.IPlugInBase.AsyncError
+  Public Event LogLine(ByVal text As String) Implements JHSoftware.SimpleDNS.Plugin.IPlugInBase.LogLine
+  Public Event SaveConfig(ByVal config As String) Implements JHSoftware.SimpleDNS.Plugin.IPlugInBase.SaveConfig
+#End Region
+
+#Region "not implemented"
+  Public Function GetDNSAskAbout() As JHSoftware.SimpleDNS.Plugin.DNSAskAbout Implements JHSoftware.SimpleDNS.Plugin.IIgnoreRequestPlugIn.GetDNSAskAbout
+  End Function
+
+  Public Function GetOptionsUI(ByVal instanceID As System.Guid, ByVal dataPath As String) As JHSoftware.SimpleDNS.Plugin.OptionsUI Implements JHSoftware.SimpleDNS.Plugin.IPlugInBase.GetOptionsUI
+    Return Nothing
+  End Function
+
+  Public Function InstanceConflict(ByVal config1 As String, ByVal config2 As String, ByRef errorMsg As String) As Boolean Implements JHSoftware.SimpleDNS.Plugin.IPlugInBase.InstanceConflict
+    Return False
+  End Function
+
+  Public Function SaveState() As String Implements JHSoftware.SimpleDNS.Plugin.IPlugInBase.SaveState
+    Return ""
+  End Function
+
+  Public Sub StartService() Implements JHSoftware.SimpleDNS.Plugin.IPlugInBase.StartService
+  End Sub
+
+  Public Sub StopService() Implements JHSoftware.SimpleDNS.Plugin.IPlugInBase.StopService
+  End Sub
+
+  Public Sub LoadConfig(ByVal config As String, ByVal instanceID As System.Guid, ByVal dataPath As String, ByRef maxThreads As Integer) Implements JHSoftware.SimpleDNS.Plugin.IPlugInBase.LoadConfig
+  End Sub
+
+  Public Sub LoadState(ByVal state As String) Implements JHSoftware.SimpleDNS.Plugin.IPlugInBase.LoadState
+  End Sub
+
+#End Region
+
+  Public Function GetPlugInTypeInfo() As JHSoftware.SimpleDNS.Plugin.IPlugInBase.PlugInTypeInfo Implements JHSoftware.SimpleDNS.Plugin.IPlugInBase.GetPlugInTypeInfo
+    With GetPlugInTypeInfo
+      .Name = "Ignore DNS Request"
+      .Description = "Instructs Simple DNS Plus to ignore (not answer) DNS requests"
+      .InfoURL = "http://www.simpledns.com/plugin-ignorereq"
+      .ConfigFile = False
+      .MultiThreaded = False
+    End With
+  End Function
+
+  Public Function IgnoreRequest(ByVal request As JHSoftware.SimpleDNS.Plugin.IDNSRequest) As Boolean Implements JHSoftware.SimpleDNS.Plugin.IIgnoreRequestPlugIn.IgnoreRequest
+    Return True
+  End Function
+
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Ignore DNS Request Plug-In")> 
+<Assembly: AssemblyDescription("Ignore DNS Request Plug-In")> 
+<Assembly: AssemblyCompany("JH Software ApS")> 
+<Assembly: AssemblyProduct("Simple DNS Plus")> 
+<Assembly: AssemblyCopyright("Copyright © 2009-2015 JH Software ApS")> 
+<Assembly: AssemblyTrademark("Simple DNS Plus")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6d7c7b0c-dbe0-4cf4-aa91-7f4f3da8d38d")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("5.2.30.0")> 
+<Assembly: AssemblyFileVersion("5.2.30.0")> 
